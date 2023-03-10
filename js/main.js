7b5703eeee7b797879687d7840edd8832d350b3b
AleksKen <aleks.ken2017@mail.ru>
2023-03-10 21:06:06 +0300

    fun

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 function getRandomInt(min, max) {
   if (max < min) {
-    console.log('Передан некорректный диапазон');
     return false;
   } else {
     return Math.floor(Math.random() * (max - min + 1)) + min;
