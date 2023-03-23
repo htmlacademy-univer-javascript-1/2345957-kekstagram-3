a2439c2539e509e1eac3bb42cb1afa222e75cee4
Keks <keks@htmlacademy.ru>
2023-03-23 21:43:27 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,9 @@
+import './data.js';
+import './until.js';
+import './render.js';
+import {getObjects} from './data.js';
+import {renderingThumbnail} from './render.js';
+
+renderingThumbnail(getObjects(25));
+import './form.js';
+import './validation.js';
---
+++ 8/js/render.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('.pictures');
+const usersPictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+function renderingThumbnail(getObjects) {
+  const usersPicturesFragment = document.createDocumentFragment();
+
+  getObjects.forEach(({url, likes, comments}) => {
+    const pictureElement = usersPictureTemplate.cloneNode(true);
+
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments;
+
+    usersPicturesFragment.appendChild(pictureElement);
+  });
+  container.appendChild(usersPicturesFragment);
+}
+
+export {renderingThumbnail};
